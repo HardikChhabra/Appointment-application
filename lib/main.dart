fc5c8f1946244503b3f6cc86e6543455be61be85
Hardik <hardikchhabra3007@gmail.com>
2024-04-25 22:26:44 +0530

    Updated Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:appointment_app/pages/appointments.dart';
-import 'package:appointment_app/pages/company_login.dart';
+import 'package:appointment_app/pages/company_home.dart';
 import 'package:appointment_app/pages/customer_home.dart';
-import 'package:appointment_app/pages/customer_login.dart';
 import 'package:appointment_app/pages/new_login.dart';
 
 import 'package:flutter/material.dart';
@@ -10,7 +9,7 @@ void main() async {
   runApp(MaterialApp(
     routes: {
       '/':(context) => const Login(),
-      '/Company':(context) => const CompanyLogin(),
+      '/Company':(context) => const CompanyHome(),
       '/Customer':(context) => const CustomerHome(),
       '/add_appointments':(context) => const Appointments(),
     },
